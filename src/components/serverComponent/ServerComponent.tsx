e436484d95f8b52d8387fbdd910aa83b252438f9
Ewa Potorecka <ewa.potorecka@thirty3.ch>
2023-06-06 21:10:02 +0200

    server component with mui button

--- src/components/serverComponent/ServerComponent.tsx
+++ src/components/serverComponent/ServerComponent.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/@mui/material";
+
 export default function NestedServer() {
   console.log("Nested server component rendering");
   return (
     <div>
       <h3>Nested Server</h3>
       <p>Nested server content</p>
+      <Button>MUI button 2</Button>
     </div>
   );
 }
